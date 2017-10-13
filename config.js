95848f33c14eb035f649b3eb38858bfb51ec332c
hkalyani <kalyani.pandala@gmail.com>
2017-10-13 18:03:04 +0530

    Initial configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAKCEZBPmNCMBABZCkwDnQ99zfCfBOkMIKuC24KutTa5U05A8YnqYSmEdEKcTXZA8kZAZAET2gtFCfwS4tfHa5nZBZCiHCMfoapvGfZBsJ6f2m70XZADhmA5OoE0iP2lyVZCdShUfZBNUcdZAuSpTCfz6acABqZCEZCZCs5SQ4jnLeeD2A7BB31D8ova9RO',
+    FB_VERIFY_TOKEN: 'mynameiskalyanipandala',
+    API_AI_CLIENT_ACCESS_TOKEN: 'b195ac1adb584b1da8a6e623148efafa',
+    FB_APP_SECRET: 'bac0b0f4de3bb9041b6476943e1513b9',
+    SERVER_URL: " https://mysterious-earth-14903.herokuapp.com/",
 };
