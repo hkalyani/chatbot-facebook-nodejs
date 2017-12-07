e013cd418b3e2602f9117e2bc84285277649c960
hkalyani <kalyani.pandala@gmail.com>
2017-12-07 12:58:03 +0530

    error fix

--- app.js
+++ app.js
@@ -929,28 +929,16 @@ function verifyRequestSignature(req, res, buf) {
 }
 
 function sendEmail(subject, content) {
-			 var helper = require('sendgrid').mail;
-
-       var from_email = new helper.Email(config.EMAIL_FROM);
-       var to_email = new helper.Email(config.EMAIL_TO);
-       var subject = subject;
-       var content = new helper.Content("text/html", content);
-       var mail = new helper.Mail(from_email, subject, to_email, content);
-
-       var sg = require('sendgrid')(config.SENDGRID_API_KEY);
-       var request = sg.emptyRequest({
-           method: 'POST',
-           path: '/v3/mail/send',
-           body: mail.toJSON()
-       });
-
-       sg.API(request, function(error, response) {
-           console.log(response.statusCode)
-           console.log(response.body)
-           console.log(response.headers)
-       })
-
-}
+	sgMail.setApiKey(config.SENDGRID_API_KEY);
+	const msg = {
+		to: config.EMAIL_TO,
+		from: config.EMAIL_FROM,
+		subject: subject,
+		text: content,
+		html: content,
+	};
+	sgMail.send(msg);
+  }
 
 
 function isDefined(obj) {
